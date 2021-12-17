c6d45f8d3fdee7304517baaa638a9cdc2fddd70b
varzar <varzar@i.ua>
2021-12-17 20:10:51 +0200

    modifided HelloGit: number of task #1

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -1,5 +1,7 @@
 public class HelloGit {
     public static void main(String[] args) {
+        int numberTask = 1;
         System.out.println("Hello Git! \nHello world!");
+        System.out.println("This is task #" + numberTask + " on the course.");
     }
 }
